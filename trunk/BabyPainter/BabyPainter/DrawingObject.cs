920061a5aebe78cea05cd86cf1a91b64c7b2a470
yeoupooh <yeoupooh@a08feccf-a647-0410-a35e-e7dac05982ae>
2008-06-15 13:11:02 +0000

    * added DrawingObject.cs.

---
+++ trunk/BabyPainter/BabyPainter/DrawingObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace BabyPainter
+{
+    public class DrawingObject
+    {
+        private Pen mPen;
+        private GraphicsPath mGp;
+
+
+        public Pen Pen
+        {
+            get { return mPen; }
+            set { mPen = value; }
+        }
+
+        public GraphicsPath GraphicsPath
+        {
+            get { return mGp; }
+            set { mGp = value; }
+        }
+
+        public DrawingObject(Pen pen, GraphicsPath gp)
+        {
+            mPen = pen;
+            mGp = gp;
+        }
+    }
+}
--- trunk/BabyPainter/BabyPainter/FormMain.cs
+++ trunk/BabyPainter/BabyPainter/FormMain.cs
@@ -6,17 +6,19 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace BabyPainter
 {
     public partial class FormMain : Form
     {
-        private float m_lineWidth = 5;
-        private Color m_lineColor = Color.Blue;
+        private float mLineWidth = 5;
+        private Color mLineColor = Color.Blue;
 
-        private Point m_prevLoc;
+        private Point mPrevLoc;
 
-        private GraphicsPath m_gp;
+        private List<DrawingObject> mDrawingObjects = new List<DrawingObject>();
+        private GraphicsPath mGp;
 
         public FormMain()
         {
@@ -32,8 +34,10 @@ namespace BabyPainter
         {
             if (e.Button == MouseButtons.Left)
             {
-                m_prevLoc = e.Location;
-                m_gp = new GraphicsPath();
+                mPrevLoc = e.Location;
+                mGp = new GraphicsPath();
+                mDrawingObjects.Add(new DrawingObject(new Pen(mLineColor, mLineWidth), mGp));
+                Invalidate();
             }
         }
 
@@ -41,14 +45,15 @@ namespace BabyPainter
         {
             if (e.Button == MouseButtons.Left)
             {
-                Graphics g = this.CreateGraphics();
-                using (Pen p = new Pen(m_lineColor, m_lineWidth))
+                //Graphics g = this.CreateGraphics();
+                mGp.AddLine(mPrevLoc, e.Location);
+                //using (Pen p = new Pen(mLineColor, mLineWidth))
                 {
-                    m_gp.AddLine(m_prevLoc, e.Location);
                     //g.DrawLine(p, m_prevLoc, e.Location);
-                    g.DrawPath(p, m_gp);
+                    //g.DrawPath(p, mGp);
                 }
-                m_prevLoc = e.Location;
+                mPrevLoc = e.Location;
+                this.Invalidate();
             }
         }
 
@@ -56,33 +61,52 @@ namespace BabyPainter
         {
             if (e.Button == MouseButtons.Left)
             {
-                Graphics g = this.CreateGraphics();
-                using (Pen p = new Pen(m_lineColor, m_lineWidth))
-                {
-                    g.DrawLine(p, m_prevLoc, e.Location);
-                }
-                m_prevLoc = e.Location;
-            }
-
-            if (m_gp != null)
-            {
-                m_gp = null;
+                mGp.AddLine(mPrevLoc, e.Location);
+                //Graphics g = this.CreateGraphics();
+                //using (Pen p = new Pen(mLineColor, mLineWidth))
+                //{
+                //    g.DrawLine(p, mPrevLoc, e.Location);
+                //}
+                //mPrevLoc = e.Location;
+                this.Invalidate();
             }
         }
 
         private void buttonNew_Click(object sender, EventArgs e)
         {
+            mDrawingObjects.Clear();
             this.Invalidate();
         }
 
         private void lineSelectBox1_LineWidthChanged(object sender, LineWidthChangedEventArgs e)
         {
-            m_lineWidth = e.LineWidth;
+            mLineWidth = e.LineWidth;
         }
 
         private void paletteSelectBox1_ColorChanged(object sender, ColorChangedEventArgs e)
         {
-            m_lineColor = e.Color;
+            mLineColor = e.Color;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap bmp = new Bitmap(this.Width, this.Height);
+                    this.DrawToBitmap(bmp, this.Bounds);
+                    bmp.Save(dlg.FileName, ImageFormat.Png);
+                }
+            }
+        }
+
+        private void buttonNew_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (DrawingObject drawingObject in mDrawingObjects)
+            {
+                e.Graphics.DrawPath(drawingObject.Pen, drawingObject.GraphicsPath);
+            }
         }
     }
 }
